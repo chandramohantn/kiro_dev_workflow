28d7e6d404225e3ed26d2fa5bd07767b56c29e0d
Chandramohan <chandramohantn23@gmail.com>
2026-03-22 01:09:15 +0530

    refactor: removed old agents

--- tools/content_generation_run.py
+++
@@ -1,6 +0,0 @@
-from skills.content_generation.scripts.run import run_content_generation
-from core.session_manager import SessionManager
-
-def run():
-    # LLM invocation wrapper
-    run_content_generation(llm_callable=None)
--- tools/drift_detection_run.py
+++
@@ -1,6 +0,0 @@
-from skills.drift_detection.scripts.run import run_drift_detection
-from core.session_manager import SessionManager
-
-def run():
-    # LLM invocation wrapper
-    run_drift_detection(llm_callable=None)
